26a847704991814c01442de2f0b6dc55ca591dbf
dieckie <dieckieus@gmail.com>
2016-05-22 18:11:57 +0200

    Enemykommentar weg

--- Enemy.java
+++ Enemy.java
@@ -9,7 +9,6 @@ import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Enemy extends Actor {
 
     public GreenfootSound fatality = new GreenfootSound("Fatality.mp3");
-    //sdfsdfsdf
     /**
      * Act - do whatever the Enemy wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
