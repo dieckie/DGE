6e0e0c8f69c926b5399d9d7ae04eed30712a7aaa
dieckie <dieckieus@gmail.com>
2016-05-22 18:27:43 +0200

    Jaja

--- download.sh
+++
@@ -1,2 +0,0 @@
-git pull origin master
-read -p "Press [Enter] key to finish..."
--- upload.sh
+++
@@ -1,6 +0,0 @@
-read -p "Commit Message: " message
-git add *
-git commit -a -m $message
-git push origin master
-echo Upload erfolgreich.
-read -p "Press [Enter] key to finish..."
