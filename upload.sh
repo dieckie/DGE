6e4ba3326fe05ca3bb160960fdb45f62a8a45897
Koljenka <ko.nagel2000@gmail.com>
2016-05-22 18:24:07 +0200

    test

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
-read -p "Commit Message" message
+read -p "Commit Message: " message
 git add *
-git commit -a -m message
+git commit -a -m $message
 git push origin master
 echo Upload erfolgreich.
 read -p "Press [Enter] key to finish..."
